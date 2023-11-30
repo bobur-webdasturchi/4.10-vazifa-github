7c0ed2c3b22fac7c9336b64679ab8a0e79f7affa
bobur-webdasturchi <boburmirzot4@gmail.com>
2023-11-30 23:40:10 +0500

    add homevork

---
+++ app.js
@@ -0,0 +1,98 @@
+
+
+//String.prototype.charAt = function () {
+//}
+//let ses = "Salom";
+//console.log(ses.charAt);
+
+//let sas = "Salom"
+//console.log(sas.charAt(0));
+
+
+//charAt ni yasash
+//String.prototype.charAt = function (i){
+//   const natija = this[i];
+//   if(natija){
+//    return natija
+//   }
+//   else{
+//    return ""
+//   }
+//}
+
+
+//at ni yasash
+//String.prototype.at = function (i) {
+//    if (i > -1) {
+//        return this[i]
+//    }
+//    else {
+//        return this[this.length +i];
+//    }
+//}
+//
+//let w  = "salom"
+//let res = w.at(-1)
+//console.log(res);
+
+
+//concat ni yasash
+//String.prototype.concat = function (i){
+//    return this.valueOf() + i;
+//}
+
+
+//let w  = "salom"
+//let res = w.concat(" dunyodagi inson va mahluqotlar")
+//console.log(res);
+//
+
+
+
+//endsWith ni yasash
+//String.prototype.endsWith = function (i){
+//
+//   if (i == this[this.length -1]) {
+//    return true
+//   } else{
+//    return false
+//   }
+//}
+//let w  = "salomsm"
+//let res = w.endsWith("m")
+//console.log(res);
+
+
+
+//String.prototype.includes = function (i){
+//    for (let a = 0; a < this.length; a++) {
+//         if (i == this[a]) {
+//            return true
+//         }
+//        
+//    }
+//    return false
+//}
+
+
+
+
+//String.prototype.indexOf = function (i,ar){
+ //   i= this.length[ar]
+   // if (i > 0) {
+    //    return true
+    //}else{
+      //  return false
+    //}
+//}
+
+//String.prototype.repeat = function (i){
+//  const assa = this.length
+//  return i * assa
+//}
+
+//String.prototype.replace = function (i){
+//  return this.length[i] + "un"
+
+//}
+
